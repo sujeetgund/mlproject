a867bee68d0af60e55ce8f56ef89fb2193e51a8b
Sujeet Gund <sujeetgund@gmail.com>
2024-04-29 12:16:24 +0530

    add data ingestion component and fix code in exception and logger modules

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,17 @@
 import sys
-from logger import logging
 
-def error_message_detail(error, error_detail: sys) -> str:
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
+def error_message_detail(error_message: str, error_detail: sys) -> str:
+    """Create a custom error message"""
+    filename = error_detail.exc_info()[2].tb_frame.f_code.co_filename
+    lineno = error_detail.exc_info()[2].tb_lineno
 
-    error_message = "Error occured in file [{0}] at line number [{1}]. Error message: [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
+    custom_message = "Error occured in file [{0}] at line number [{1}]. Error message: [{2}]".format(filename, lineno, error_message)
 
-    return error_message
+    return custom_message
 
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message: str, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error=error_message, error_detail=error_detail)
 
--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+# Create a unique file name
+LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Create directory where log files will be saved
+DIR_PATH = os.path.join(os.getcwd(), 'logs', LOG_FILE_NAME)
+os.makedirs(DIR_PATH, exist_ok=True)
 
+# Create a log file path
+LOG_FILE_PATH = os.path.join(DIR_PATH, LOG_FILE_NAME)
+
+# Logging config
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
